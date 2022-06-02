0e6730f0806db1b8144079effb7efac961e1d59d
Iris <marlx42@protonmail.com>
2022-06-02 11:39:16 +0200

    Afficher e-mail

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -21,17 +21,20 @@ class EmailsController < ApplicationController
 
   # POST /emails or /emails.json
   def create
-    @email = Email.new(email_params)
+    @email = Email.create(object: Faker::Book.title,body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4))
 
     respond_to do |format|
       if @email.save
-        format.html { redirect_to email_url(@email), notice: "Email was successfully created." }
-        format.json { render :show, status: :created, location: @email }
+        format.html { redirect_to emails_path}
+        # format.json { render :show, status: :created, location: @email }
+        format.js{}
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @email.errors, status: :unprocessable_entity }
       end
     end
+
+    
   end
 
   # PATCH/PUT /emails/1 or /emails/1.json
